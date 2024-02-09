45480f07c2751f3c37f7f2d88d8c257ca5270e56
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-02-09 15:39:18 -0500

    Add artLabel

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const base_url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/459123"
+const base_url =
+  'https://collectionapi.metmuseum.org/public/collection/v1/objects/459123';
 
-function initalFetch(){
-    fetch(base_url)
-    .then((res)=> res.json())
-    .then((artObj)=>{
-        let artDiv = document.querySelector("#mainArt")
-       let artImg = document.createElement("img")
-       artImg.src = artObj.primaryImage
-        artDiv.append(artImg)
-        
-    })
+function initalFetch() {
+  fetch(base_url)
+    .then((res) => res.json())
+    .then((artObj) => {
+      console.log(artObj);
+      let artDiv = document.querySelector('#mainArt');
+      let artImg = document.createElement('img');
+      artImg.src = artObj.primaryImage;
+      artDiv.append(artImg);
+      artDiv.append(createLabel(artObj));
+    });
 }
 
-initalFetch()
+initalFetch();
+
+function createLabel(artObj) {
+  const artLabel = document.createElement('div');
+  artLabel.id = 'artLabel';
+  let title = document.createElement('h2');
+  title.textContent = artObj.title;
+  let name = document.createElement('h3');
+  name.textContent = artObj.artistDisplayName;
+  artLabel.append(title, name);
+  return artLabel;
+}
