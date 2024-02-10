c81a6a31974fdbacdce763f5b759c05bd75df7d0
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-02-10 11:33:47 -0500

    Add department selectbox

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const landingImg = [459121, 459122, 459123, 459125, 459126, 459127]
+const landingImg = [459121, 459122, 459123, 459125, 459126, 459127];
 
-const base_url =`https://collectionapi.metmuseum.org/public/collection/v1/objects/`;
+const base_url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/`;
 
 function initalFetch() {
-// landing page Loop of Images
+  // landing page Loop of Images
 
-function pullRando(landingImg){
-  let index = Math.floor(Math.random() * landingImg.length)
-  return landingImg[index]
-}
-
+  function pullRando(landingImg) {
+    let index = Math.floor(Math.random() * landingImg.length);
+    return landingImg[index];
+  }
 
   fetch(`${base_url}${pullRando(landingImg)}`)
     .then((res) => res.json())
@@ -23,8 +22,6 @@ function pullRando(landingImg){
     });
 }
 
-initalFetch();
-
 function createLabel(artObj) {
   const artLabel = document.createElement('div');
   artLabel.id = 'artLabel';
@@ -37,3 +34,49 @@ function createLabel(artObj) {
   artLabel.append(title, name, artistBio);
   return artLabel;
 }
+
+function createDepartmentInput() {
+  const departments = [
+    { departmentId: 1, displayName: 'American Decorative Arts' },
+    { departmentId: 3, displayName: 'Ancient Near Eastern Art' },
+    { departmentId: 4, displayName: 'Arms and Armor' },
+    {
+      departmentId: 5,
+      displayName: 'Arts of Africa, Oceania, and the Americas',
+    },
+    { departmentId: 6, displayName: 'Asian Art' },
+    { departmentId: 7, displayName: 'The Cloisters' },
+    { departmentId: 8, displayName: 'The Costume Institute' },
+    { departmentId: 9, displayName: 'Drawings and Prints' },
+    { departmentId: 10, displayName: 'Egyptian Art' },
+    { departmentId: 11, displayName: 'European Paintings' },
+    {
+      departmentId: 12,
+      displayName: 'European Sculpture and Decorative Arts',
+    },
+    { departmentId: 13, displayName: 'Greek and Roman Art' },
+    { departmentId: 14, displayName: 'Islamic Art' },
+    { departmentId: 15, displayName: 'The Robert Lehman Collection' },
+    { departmentId: 16, displayName: 'The Libraries' },
+    { departmentId: 17, displayName: 'Medieval Art' },
+    { departmentId: 18, displayName: 'Musical Instruments' },
+    { departmentId: 19, displayName: 'Photographs' },
+    { departmentId: 21, displayName: 'Modern Art' },
+  ];
+  const departmentInput = document.querySelector('#departmentInput');
+  departments.forEach((department) => {
+    let option = document.createElement('option');
+    [option.value, option.textContent] = [
+      department.departmentId,
+      department.displayName,
+    ];
+    departmentInput.append(option);
+  });
+}
+
+function buildSearchForm() {
+  createDepartmentInput();
+}
+
+initalFetch();
+buildSearchForm();
